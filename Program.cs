70cfee77e5ea737500ab22757042477b24a6838f
Alexey <brech_36@mail.ru>
2023-01-18 16:54:04 +0900

    add min and max

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт,
+ //какое число большее, а какое меньшее.
+
+Console.Write("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите первое число:");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a>b)
+{
+    Console.Write($"{a}==max, {b}==min");
+    };
+if (a<b)
+{
+    Console.Write($"{b}==max, {a}==min");
+};
